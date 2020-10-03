eefc674e0fe17c2068d0b92b4fbcf6376bbb4427
Vincent Wang <vwang00@gmail.com>
2020-10-04 00:15:16 +0100

    Add intervals to chord related enums

--- src/Enums/Extension.java
+++ src/Enums/Extension.java
@@ -1,14 +1,20 @@
 package Enums;
 
 public enum Extension {
-  NONE(""),
-  MIN7("7"),
-  MAJ7("maj7");
+  NONE("", null),
+  MIN7("7", Interval.MIN7),
+  MAJ7("maj7", Interval.MAJ7);
 
   private String rep;
+  private Interval interval;
 
-  Extension(String rep) {
+  Extension(String rep, Interval interval) {
     this.rep = rep;
+    this.interval = interval;
+  }
+
+  public Interval getInterval() {
+    return interval;
   }
 
   @Override
--- src/Enums/Triad.java
+++ src/Enums/Triad.java
@@ -1,13 +1,23 @@
 package Enums;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum Triad {
-  MAJ(""),
-  MIN("-");
+  MAJ("", new HashSet<Interval>(Arrays.asList(Interval.MAJ3, Interval.PERF5))),
+  MIN("-", new HashSet<Interval>(Arrays.asList(Interval.MIN3, Interval.PERF5))),
+  DIM("o", new HashSet<Interval>(Arrays.asList(Interval.MIN3, Interval.AUG4)));
 
   private String rep;
+  private Set<Interval> intervals;
 
-  Triad(String rep) {
+  Triad(String rep, Set<Interval> intervals) {
     this.rep = rep;
+    this.intervals = intervals;
+  }
+
+  public Set<Interval> getIntervals() {
+    return intervals;
   }
 
   @Override
